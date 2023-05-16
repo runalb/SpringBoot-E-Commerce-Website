ae8d49a8f22fbd3aa50b450f55d46ed9c24373c7
runalb <65166862+runalb@users.noreply.github.com>
2023-05-16 16:02:45 +0530

    Security

---
+++ src/main/java/com/runalb/major/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.runalb.major.repository;
+
+import com.runalb.major.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
